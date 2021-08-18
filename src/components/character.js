4faf98d0a5b250699ed2eb42f72348f4994aea0e
Md.Usman Ansari <40880948+MdUsmanAnsari@users.noreply.github.com>
2021-08-18 21:43:40 +0530

    Error solved

--- src/components/character.js
+++ src/components/character.js
@@ -10,29 +10,36 @@ const Character = () => {
     const userLink = `https://breakingbadapi.com/api/characters/${charId}`;
     useEffect(() => {
         fetch(userLink)
-        .then(res => res.json())
-        .then(data => setCharacter(data[0]));
+            .then(res => res.json())
+            .then(data => setCharacter(data[0]));
     }, []);
 
-    const quotesLink = `https://breakingbadapi.com/api/quote?author=${character.name.replaceAll(" ", "+")}`;
+
     useEffect(() => {
-        
+
+        if (character.name === undefined) return;
+
+        const quotesLink = `https://breakingbadapi.com/api/quote?author=${character.name.replaceAll(" ", "+")}`;
+
         fetch(quotesLink)
-        .then(res => res.json())
-        .then(data => setQuotes(data));
+            .then(res => res.json())
+            .then(data => setQuotes(data));
+
+
     }, [character]);
 
-    return(
+    return (
         <div>
-            <h1>{ character.name } </h1>
-            <p>{ character.birthday } </p>
-            <p>{ character.occupation } </p>
-            <p>{ character.status } </p>
-            <p>{ character.nickname } </p>
-            <p>{ character.appearance } </p>
-            <p>{ character.portrayed } </p>
+            <h1>{character.name} </h1>
+            <p>{character.birthday} </p>
+            <p>{character.occupation} </p>
+            <p>{character.status} </p>
+            <p>{character.nickname} </p>
+            <p>{character.appearance} </p>
+            <p>{character.portrayed} </p>
             <ul>
-                {quotes}
+                {/* {map the all quotes here */}
+                {/* {quotes} */}
             </ul>
         </div>
     )
